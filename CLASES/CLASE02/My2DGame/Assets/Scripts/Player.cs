3ae5d63726f6916e041cf6cce3c03e9fc1806220
Alejandro <alejandrovm@gmail.com>
2019-12-03 17:18:09 -0500

    classes files added

---
+++ CLASES/CLASE02/My2DGame/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print ("hola mundo");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(1f * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
