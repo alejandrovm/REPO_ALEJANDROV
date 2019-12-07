3fb8fa140182b628f45b4e7e2a4db5e749c3aab3
alejandrovm <alejandrovm@gmail.com>
2019-12-07 11:36:48 -0500

    Text Adventure
    New Visual Project: Text Adventure in C#

---
+++ PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextAdventure
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
