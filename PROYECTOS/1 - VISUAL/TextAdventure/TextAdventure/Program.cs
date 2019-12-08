a002c2184b6089faa5e10b4bab05c5c608942d75
alejandrovm <alejandrovm@gmail.com>
2019-12-08 18:49:45 -0500

    Update to program
    not much but something is something...

--- PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
+++ PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿/*
+THE LOST MINE
+by Alejandro Villalobos
+
+This is a very loose adaptation of the 1st part of The Lost Mine of Phandelver,
+titled Goblin Arrows, a Wizards of the Coast module for D&D 5e Begginers
+ */
+
+using System;
 
 namespace TextAdventure
 {
-    class MainClass
+    class Game
+    {
+    }
+    class Item
+    {
+    }
+    class Program
     {
         static void Main()
         {
@@ -14,7 +28,7 @@ namespace TextAdventure
             Console.WriteLine("Name your character:");
 
             CharacterName = Console.ReadLine();
-            Console.WriteLine("Alright then, your character's name is: " + CharacterName);
+            Console.WriteLine("Alright then, your character's name is: " + CharacterName + ". Good Luck " + CharacterName + "! (you'll need it)");
 
             Console.ReadKey();
         }
