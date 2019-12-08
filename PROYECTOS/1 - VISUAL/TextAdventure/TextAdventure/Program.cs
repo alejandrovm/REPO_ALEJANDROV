69719f0276d05a19808a127a1dba976b0429c7d5
alejandrovm <alejandrovm@gmail.com>
2019-12-08 17:36:06 -0500

    Slowly advancing with project
    Is going to be a text adventure game... hopefully.

--- PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
+++ PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
@@ -4,9 +4,18 @@ namespace TextAdventure
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Adventure Game!");
+            string CharacterName = "";
+
+            Console.WriteLine("A Text Adventure Game");
+            Console.WriteLine("by Alejandro Villalobos");
+            Console.WriteLine("THE LOST MINE");
+            Console.WriteLine("Name your character:");
+
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Alright then, your character's name is: " + CharacterName);
+
             Console.ReadKey();
         }
     }
