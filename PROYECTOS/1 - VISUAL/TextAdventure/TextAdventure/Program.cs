d25d82b29756a6e7b9de7a71de0a655d05aeca85
alejandrovm <alejandrovm@gmail.com>
2019-12-09 00:29:20 -0500

    Kept working on project
    not all who wander are lost, but I sure am

--- PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
+++ PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
@@ -2,16 +2,35 @@
 THE LOST MINE
 by Alejandro Villalobos
 
-This is a very loose adaptation of the 1st part of The Lost Mine of Phandelver,
-titled Goblin Arrows, a Wizards of the Coast module for D&D 5e Begginers
+The following is a very loose adaptation of the 1st part of 'The Lost Mine of Phandelver',
+entitled 'Goblin Arrows', a 'Wizards of the Coast' module for D&D 5e.
  */
 
 using System;
 
 namespace TextAdventure
 {
-    class Game
+    public static class Game
     {
+        //character name
+        static string CharacterName = "";
+
+        //print out game title and overview
+        public static void StartGame()
+        {
+            Console.WriteLine("A Text Adventure Game");
+            Console.WriteLine("by Alejandro Villalobos");
+            Console.WriteLine("THE LOST MINE");
+            NameCharacter();
+        }
+        //ask player for a name, and save it
+        static void NameCharacter()
+        {
+            Console.WriteLine("Name your character:");
+
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Alright then, your character's name is: " + CharacterName + ". Good Luck " + CharacterName + "! (you'll need it)");
+        }
     }
     class Item
     {
@@ -20,16 +39,7 @@ namespace TextAdventure
     {
         static void Main()
         {
-            string CharacterName = "";
-
-            Console.WriteLine("A Text Adventure Game");
-            Console.WriteLine("by Alejandro Villalobos");
-            Console.WriteLine("THE LOST MINE");
-            Console.WriteLine("Name your character:");
-
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("Alright then, your character's name is: " + CharacterName + ". Good Luck " + CharacterName + "! (you'll need it)");
-
+            Game.StartGame();
             Console.ReadKey();
         }
     }
