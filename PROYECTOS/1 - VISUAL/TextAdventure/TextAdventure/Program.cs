c6aada6bbca2bb88f24f9cf3de7f2ee64e9dd7dc
alejandrovm <alejandrovm@gmail.com>
2019-12-09 15:19:38 -0500

    Progress in the project was made... I think...
    Story started

--- PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
+++ PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
@@ -30,6 +30,11 @@ namespace TextAdventure
 
             CharacterName = Console.ReadLine();
             Console.WriteLine("Alright then, your character's name is: " + CharacterName + ". Good Luck " + CharacterName + "! (you'll need it)");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("In the city of Neverwinter, a dwarf named Gundren Rockseeker asked you to bring a wagon load of provisions to the rough-and-tumble settlement of Phandalin, a couple of days' travel southeast of the city. Gundren was clearly excited and more than a little secretive about his reasons for the trip, saying only that he and his brothers had found 'something big', and that he'd pay you ten gold pieces for escorting his supplies safely to Barthen's Provisions, a trading post in Phandalin.He then set out ahead of you on horse, along with a warrior escort named Sildar Hallwinter, claiming he needed to arrive early to 'take care of business'.");
+            Console.ReadKey();
+            Console.WriteLine("You've been on the Triboar Trail for about half a day. As you come around a bend, you spot two dead horses sprawled about fifty feet ahead of you, blocking the path. Each has several black-feathered arrows sticking out of it. The woods press close to the trail here, with a steep embankment and dense thickets on either side.");
         }
     }
     class Item
