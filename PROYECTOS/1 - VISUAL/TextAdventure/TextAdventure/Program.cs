b42a756cb064a6bdffd26b3889338531b89c1841
alejandrovm <alejandrovm@gmail.com>
2019-12-09 17:12:00 -0500

    added title
    added title, might change later

--- PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
+++ PROYECTOS/1 - VISUAL/TextAdventure/TextAdventure/Program.cs
@@ -18,9 +18,44 @@ namespace TextAdventure
         //print out game title and overview
         public static void StartGame()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string title = @"
+
++-------------------------------------------------------------------------------------+
+|                                                                                     |
+|                            XXXXXXXXX  XXX   XXX  XXXXXXXXX                          |
+|                            XXXXXXXXX  XXX   XXX  XXXXXXXXX                          |
+|                               XXX     XXX   XXX  XXX                                |
+|                               XXX     XXXXXXXXX  XXXXXX                             |
+|                               XXX     XXXXXXXXX  XXXXXX                             |
+|                               XXX     XXX   XXX  XXX                                |
+|                               XXX     XXX   XXX  XXXXXXXXX                          |
+|                               XXX     XXX   XXX  XXXXXXXXX                          |
+|                                                                                     |
+|                                                                                     |
+|   XXX      XXXXXXXXX  XXXXXXXXX  XXXXXXXXX   XX     XX  XXX  XX     XX  XXXXXXXXX   |
+|   XXX      XXXXXXXXX  XXXXXXXXX  XXXXXXXXX   XXX   XXX  XXX  XXX    XX  XXXXXXXXX   |
+|   XXX      XXXXXXXXX  XX            XXX      XXXX XXXX  XXX  XXXX   XX  XXX         |
+|   XXX      XXX   XXX  XXXXXXXXX     XXX      XX XXX XX  XXX  XX XX  XX  XXXXXX      |
+|   XXX      XXX   XXX  XXXXXXXXX     XXX      XX  X  XX  XXX  XX  XX XX  XXXXXX      |
+|   XXXXXXX  XXXXXXXXX         XX     XXX      XX     XX  XXX  XX   XXXX  XXX         |
+|   XXXXXXX  XXXXXXXXX  XXXXXXXXX     XXX      XX     XX  XXX  XX    XXX  XXXXXXXXX   |
+|   XXXXXXX  XXXXXXXXX  XXXXXXXXX     XXX      XX     XX  XXX  XX     XX  XXXXXXXXX   |
+|                                                                                     |
+|                                                                                     |
+|                                 By Alejandro Villalobos                             |
+|                                                                                     |
++-------------------------------------------------------------------------------------+
+
+";
+
+            Console.WriteLine(title);
             Console.WriteLine("A Text Adventure Game");
             Console.WriteLine("by Alejandro Villalobos");
-            Console.WriteLine("THE LOST MINE");
+            Console.ResetColor();
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
             NameCharacter();
         }
         //ask player for a name, and save it
@@ -30,9 +65,11 @@ namespace TextAdventure
 
             CharacterName = Console.ReadLine();
             Console.WriteLine("Alright then, your character's name is: " + CharacterName + ". Good Luck " + CharacterName + "! (you'll need it)");
+            Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             Console.Clear();
             Console.WriteLine("In the city of Neverwinter, a dwarf named Gundren Rockseeker asked you to bring a wagon load of provisions to the rough-and-tumble settlement of Phandalin, a couple of days' travel southeast of the city. Gundren was clearly excited and more than a little secretive about his reasons for the trip, saying only that he and his brothers had found 'something big', and that he'd pay you ten gold pieces for escorting his supplies safely to Barthen's Provisions, a trading post in Phandalin.He then set out ahead of you on horse, along with a warrior escort named Sildar Hallwinter, claiming he needed to arrive early to 'take care of business'.");
+            Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             Console.WriteLine("You've been on the Triboar Trail for about half a day. As you come around a bend, you spot two dead horses sprawled about fifty feet ahead of you, blocking the path. Each has several black-feathered arrows sticking out of it. The woods press close to the trail here, with a steep embankment and dense thickets on either side.");
         }
