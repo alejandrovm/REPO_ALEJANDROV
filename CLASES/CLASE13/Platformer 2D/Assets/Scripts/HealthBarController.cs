cccd36730039df206e4ca23e241349384a1dda85
Alejandro V <alejandrovm@gmail.com>
2020-01-14 21:59:43 -0500

    Class 13 : Health Bar v3

---
+++ CLASES/CLASE13/Platformer 2D/Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    private float width;
+
+    public RectTransform bar;
+    public RectTransform barMiddle;
+
+    public float maxLife = 10;
+    public float _currentLife;
+
+    public float currentLife
+    {
+        set {
+            _currentLife = Mathf.Clamp (value, 0, maxLife);
+            UpdateBar ();
+        }
+
+        get {
+            return _currentLife;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = barMiddle.sizeDelta.x; 
+        _currentLife = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.I))
+            ClearLife ();
+    }
+
+    void UpdateBar ()
+    {
+        ClearLife ();
+        AddLife ();
+    }
+
+    void ClearLife ()
+    {
+        for (int i = 0; i < bar.childCount; i++)
+            Destroy (bar.GetChild(i).gameObject);
+    }
+
+    void AddLife ()
+    {
+        for (int i = 0; i < currentLife; i++) {
+            RectTransform point = Instantiate<RectTransform> (barMiddle);
+            point.parent = bar;
+            point.localScale = Vector3.one;
+            point.localPosition = new Vector3 (width * i, 0, 0);
+        }
+    }
+
+    void RemoveLife ()
+    {
+        Destroy ((bar.GetChild(bar.childCount - 1).gameObject));
+    }
+}
