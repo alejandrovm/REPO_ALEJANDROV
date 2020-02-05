1c659d681999abc7ed816be646ca7f7a9fa76425
Alejandro V <alejandrovm@gmail.com>
2020-02-04 21:56:41 -0500

    class 18, enemy movement

--- CLASES/CLASE16/Topdown 2D/Assets/Scripts/Patrol.cs
+++ CLASES/CLASE16/Topdown 2D/Assets/Scripts/Patrol.cs
@@ -6,22 +6,58 @@ namespace Topdown
 {
     public class Patrol : MonoBehaviour
 {
+        int currentIndex;
+        float t = 0f;
+        Vector3 startPos;
+        Vector3 endPos;
+        Vector3 point;
+        Vector3 origin;
         public Vector2[] points;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+            currentIndex = 0;
+            point = points[currentIndex];
+            origin = startPos;
+            startPos = transform.position;
+            endPos = startpos + point;
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            t += Time.deltatime;
+
+            if (t >= 1f)
+            {
+                startpos = endPos;
+                currentIndex++;
+
+                if (currentIndex >= points.Lenght)
+                {
+                    currentIndex = -1;
+                    point = Vector3.zero;
+                }
+                else
+                {
+
+                }
+               
+                endPos = origin + point;
+
+                t = 0;
+            }
+                
     }
 
     private void OnDrawGizmos()
     {
+            if (startpos != transform.position && !Application.IsPlaying)
+                startpos = transform.position;
+
         Vector3 point = points[0];
         Debug.DrawLine(transform.position, transform.position + point, Color.cyan);
         
