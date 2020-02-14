3b15ea6c11bc5a5d018bade4718ec6e19af66063
Alejandro V <alejandrovm@gmail.com>
2020-02-13 20:44:26 -0500

    sigh..

---
+++ CLASES/CLASE16/Topdown 2D/Assets/Scripts/AttackModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Topdown
+{
+    public class AttackModel : MonoBehaviour
+    {
+        [Serializable]
+        public struct Attacks
+        {
+            public ElementType type;
+            public Projectile prefab;
+        }
+        public Attacks[] attacksArray;
+
+        public Dictionary<ElementType, Projectile> attacks;
+
+        public int currentAttack;
+
+        void Start()
+        {
+            attacks = new Dictionary<ElementType, Projectile>();
+            foreach (Attacks a in attacksArray)
+            {
+                attacks.Add(a.type, a.prefab);
+            }
+        }
+    }
+}
