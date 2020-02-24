0d465f8d544ed9b1ae09a9ab39702fdf8e381ff0
Alejandro V <alejandrovm@gmail.com>
2020-02-23 19:51:27 -0500

    Reworked project
    2D Platformer

---
+++ PROYECTOS/2 - UNITY2D/My 2D Platformer Project/Assets/Scripts/CharController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyPlatformer
+{
+    public class CharController : MonoBehaviour
+    {
+        static public CharController _instance;
+
+        [SerializeField]
+        private float _speed;
+        [SerializeField]
+        private float _jump;
+        private SpriteRenderer spriteRenderer;
+        private Rigidbody2D _rigidbody2D;
+        private bool onGround = false;
+
+        public Vector3 startPosition;
+        public Animator animator;
+        public bool grounded
+        {
+            get
+            {
+                return RoundAbsoluteToZero(_rigidbody2D.velocity.y) == 0f || onGround;
+            }
+        }
+
+        void Awake()
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.position = startPosition;
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(new Vector3(horizontalInput, 0, 0) * _speed * Time.deltaTime);
+            animator.SetBool("Speed", (horizontalInput != 0));
+            animator.SetBool("IsJumping", !grounded);
+            animator.SetFloat("vertical", Mathf.Sign(_rigidbody2D.velocity.y));
+
+            if (horizontalInput != 0)
+            {
+                spriteRenderer.flipX = (horizontalInput < 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                _speed *= 2;
+            }
+
+            if (grounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody2D.AddForce(Vector2.up * _jump, ForceMode2D.Impulse);
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.tag == "DeathZone")
+            {
+                transform.position = startPosition;
+            }
+
+            if (col.gameObject.tag == "Floor")
+            {
+                onGround = true;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D col)
+        {
+            if (col.gameObject.tag == "Floor")
+            {
+                onGround = false;
+            }
+        }
+
+            float RoundAbsoluteToZero(float decimalValue)
+        {
+            decimalValue = Mathf.Abs(decimalValue);
+            if (decimalValue <= 0.01f)
+            {
+                decimalValue = 0f;
+            }
+            return decimalValue;
+        }
+    }
+}
---
+++ PROYECTOS/2 - UNITY2D/My 2D Platformer Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyPlatformer
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Horizontal Movement")]
+        public float _speed = 10f;
+        public Vector2 direction;
+        private bool facingRight = true;
+
+        [Header("Vertical Movement")]
+        public float _jump = 15f;
+        public float _delay = 0.25f;
+        public float _timer;
+
+        [Header("Components")]
+        public Rigidbody2D _rigidbody2D;
+        public Animator animator;
+        public LayerMask floorLayer;
+
+        [Header("Physics")]
+        public float maxSpeed = 7f;
+        public float linearDrag = 4f;
+        public float gravity = 1f;
+        public float fallMultiplier = 5f;
+
+        [Header("Collision")]
+        public bool onGround = false;
+        public float groundLength = 0.6f;
+        public Vector3 colliderOffset;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, floorLayer) || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, floorLayer); ;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                _timer = Time.time + _delay;
+            }
+
+            direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            animator.SetFloat("horizontal", Mathf.Abs(Input.GetAxis("Horizontal")));
+            animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        }
+
+        void FixedUpdate()
+        {
+            moveCharacter(direction.x);
+
+            if (_timer > Time.time && onGround)
+            {
+                Jump();
+            }
+
+            modifyPhysics();
+        }
+
+        void moveCharacter(float horizontal)
+        {
+            _rigidbody2D.AddForce(Vector2.right * horizontal * _speed);
+
+            animator.SetFloat("horizontal", Mathf.Abs(_rigidbody2D.velocity.x));
+            animator.SetFloat("vertical", _rigidbody2D.velocity.y);
+
+            if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight))
+            {
+                Flip();
+            }
+
+            if (Mathf.Abs(_rigidbody2D.velocity.x) > maxSpeed)
+            {
+                _rigidbody2D.velocity = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x) * maxSpeed, _rigidbody2D.velocity.y);
+            }
+        }
+
+        void Jump()
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);
+            _rigidbody2D.AddForce(Vector2.up * _jump, ForceMode2D.Impulse);
+            _timer = 0;
+        }
+
+        void modifyPhysics()
+        {
+            bool changingDirections = (direction.x > 0 && _rigidbody2D.velocity.x < 0) || (direction.x < 0 && _rigidbody2D.velocity.x > 0);
+
+            if (onGround)
+            {
+                if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+                {
+                    _rigidbody2D.drag = linearDrag;
+                }
+                else
+                {
+                    _rigidbody2D.drag = 0f;
+                }
+                _rigidbody2D.gravityScale = 0;
+            }
+            else
+            {
+                _rigidbody2D.gravityScale = gravity;
+                _rigidbody2D.drag = linearDrag * 0.15f;
+                if (_rigidbody2D.velocity.y < 0)
+                {
+                    _rigidbody2D.gravityScale = gravity * fallMultiplier;
+                }
+                else if (_rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+                {
+                    _rigidbody2D.gravityScale = gravity * (fallMultiplier / 2);
+                }
+            }
+}
+
+        void Flip()
+        {
+            facingRight = !facingRight;
+            transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position + colliderOffset, transform.position + colliderOffset + Vector3.down * groundLength);
+            Gizmos.DrawLine(transform.position - colliderOffset, transform.position - colliderOffset + Vector3.down * groundLength);
+        }
+    }
+}
