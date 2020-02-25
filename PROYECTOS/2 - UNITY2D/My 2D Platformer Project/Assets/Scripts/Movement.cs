eb61a070f0a07df4b5bd78b4def7ce9b6890b5b0
Alejandro V <alejandrovm@gmail.com>
2020-02-25 15:37:49 -0500

    Project 2D Platformer Finished

---
+++ PROYECTOS/2 - UNITY2D/My 2D Platformer Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace My2DPlatformer
+{
+    public class Movement : MonoBehaviour
+    {
+        public float speed = 1f;
+
+        public Vector2 startPosition;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.position = startPosition;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position += new Vector3(0, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0);
+            transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0);
+        }
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.tag == "ToLevel1")
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
