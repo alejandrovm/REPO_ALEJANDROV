c68dddf859af0c3cf87aa33e45dd4cb160caa2cc
Alejandro V <alejandrovm@gmail.com>
2020-02-27 22:10:04 -0500

    Class 3D camera

---
+++ CLASES/TowerDefense/Assets/Scripts/FPTD/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPTD
+{
+    public class CameraController : MonoBehaviour
+    {
+        public bool invert = false;
+        public float smooth = 10f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            float angle = Input.GetAxis("Mouse Y");
+            angle *= smooth * Time.deltaTime;
+
+            if (invert)
+                angle *= -1;
+            transform.Rotate(Vector3.right, angle);
+        }
+    }
+}
